e344a40abd19aecc11f29415c916198ac7a77467
Ben Chatelain <ben@ditto.live>
2022-01-30 09:55:22 -0700

    ☠️ Add DittoSwiftApp project

---
+++ DittoSwiftApp/DittoSwiftApp/DittoSwiftAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DittoSwiftAppApp.swift
+//  DittoSwiftApp
+//
+//  Created by Ben Chatelain on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct DittoSwiftAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
