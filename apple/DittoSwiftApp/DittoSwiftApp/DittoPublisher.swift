a9687b0bb0957e370eba63a23a293046ebe5e1cc
Ben Chatelain <ben@octop.ad>
2023-01-23 14:02:52 -0700

    ⚗️ Close/reopen Ditto in same dir

--- apple/DittoSwiftApp/DittoSwiftApp/DittoPublisher.swift
+++ apple/DittoSwiftApp/DittoSwiftApp/DittoPublisher.swift
@@ -15,7 +15,11 @@ class DittoPublisher: Publisher {
     var ditto: Ditto?
 
     init() {
-        ditto = Ditto()
+        let identity: DittoIdentity = .onlineWithAuthentication(appID: "46e08559-4388-4896-ba19-c1573509bb29", authenticationDelegate: self)
+        ditto = Ditto(identity: identity)
+        ditto?.auth?.logout()
+        ditto = nil
+        ditto = Ditto(identity: identity)
         try! ditto?.startSync()
     }
 
@@ -29,7 +33,15 @@ class DittoPublisher: Publisher {
 //    func subscribe(_ subscriber: Subscriber) {}
 
 
-
-
 //    func subscribe(_ subscriber: Subscriber) -> AnyCancellable {}
+}
+
+extension DittoPublisher: DittoAuthenticationDelegate {
+    func authenticationRequired(authenticator: DittoSwift.DittoAuthenticator) {
+        debugPrint("authenticationRequired")
+    }
+
+    func authenticationExpiringSoon(authenticator: DittoSwift.DittoAuthenticator, secondsRemaining: Int64) {
+        debugPrint("authenticationExpiringSoon")
+    }
 }
